d1392bb5d1539208beb7eff0aa2d6f90897e8046
Sylvain Corlay <sylvain.corlay@gmail.com>
2018-04-06 15:51:09 +0200

    Release 0.5.0

--- include/xplot/xplot_config.hpp
+++ include/xplot/xplot_config.hpp
@@ -22,8 +22,8 @@
 
 // Project version
 #define XPLOT_VERSION_MAJOR 0
-#define XPLOT_VERSION_MINOR 4
-#define XPLOT_VERSION_PATCH 1
+#define XPLOT_VERSION_MINOR 5
+#define XPLOT_VERSION_PATCH 0
 
 // Binary version
 #define XPLOT_BINARY_CURRENT 1
